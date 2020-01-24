0a11dade5efdf242e262663a7717fd66e56635a6
Romain Coeur <coeurrogmail.com>
2020-01-24 17:02:08 +0100

    fix notification issue and maj SE user to fit DB arch

--- functions/index.js
+++ functions/index.js
@@ -7,7 +7,7 @@ admin.initializeApp()
 
 
 exports.setTripFilters = functions.firestore.document('trips/{tripId}').onWrite((change, context) => setupFilters(['from', 'to'], change))
-exports.setContactFilters = functions.firestore.document('users/{userId}').onWrite((change, context) => setupFilters(['firstname', 'lastname', 'username'], change))
+exports.setContactFilters = functions.firestore.document('USERS/{userId}').onWrite((change, context) => setupFilters(['firstname', 'lastname', 'username'], change))
 
 
 /**
@@ -157,27 +157,24 @@ exports.sendNewMessageNotification = functions.firestore
 
     console.log('We have a new message. With id:', snap.id, ' and in group: ', context.params.groupId)
 
-    // Add event to unread events
+    // Get the list of participants
     const users = await admin.firestore()
       .collection(`TBS/AllCampus/AllChatGroups/${context.params.groupId}/Participants`)
       .get()
     console.log("Users : ", users.size)
-    users.forEach(doc =>
-      admin.firestore()
-        .collection(`USERS/${doc.id}/UnreadMessages`)
-        .doc(snap.id).set({message: `TBS/AllCampus/AllChatGroups/${context.params.groupId}/Chats/${snap.id}`})
-    )
-
-    // Get the list of device notification tokens.
-    const tokensSnapshot = await admin.firestore()
-      .collection(`TBS/AllCampus/AllChatGroups/${context.params.groupId}/NotificationTokens`)
-      .get()
 
     // Check if there are any device tokens.
-    if (tokensSnapshot.empty) {
+    if (users.empty) {
       return console.log('There are no notification tokens to send to.');
     }
-    console.log('There are', tokensSnapshot.size, 'tokens to send notifications to.');
+
+    // Add event to unread events
+    users.forEach(doc => {
+      if (doc.id !== snap.get('senderId'))
+        admin.firestore()
+          .collection(`USERS/${doc.id}/UnreadMessages`)
+          .doc(snap.id).set({message: `TBS/AllCampus/AllChatGroups/${context.params.groupId}/Chats/${snap.id}`})
+    })
 
     // Notification details.
     const payload = {
@@ -188,22 +185,14 @@ exports.sendNewMessageNotification = functions.firestore
     };
 
     // Listing all tokens as an array.
-    let tokens = _.map(tokensSnapshot.docs, doc => doc.id);
-    console.log(tokens)
-    // Send notifications to all tokens.
-    const response = await admin.messaging().sendToDevice(tokens, payload);
-    // For each message check if there was an error.
-    const tokensToRemove = [];
-    response.results.forEach((result, index) => {
-      const error = result.error;
-      if (error) {
-        console.error('Failure sending notification to', tokens[index], error);
-        // Cleanup the tokens who are not registered anymore.
-        if (error.code === 'messaging/invalid-registration-token' ||
-          error.code === 'messaging/registration-token-not-registered') {
-          tokensToRemove.push(tokensSnapshot.ref.child(tokens[index]).remove());
-        }
+    let tokens = await _.reduce( users.docs, async (acc, doc) => {
+      if (doc.id !== snap.get('senderId')) {
+        let tokenDocs = await admin.firestore().collection(`USERS/${doc.id}/NotificationTokens`).listDocuments()
+        tokenDocs.forEach(doc => acc.push(doc.id))
       }
-    });
-    return Promise.all(tokensToRemove);
+      return acc
+    }, [])
+
+    // Send notifications to all tokens.
+    return admin.messaging().sendToDevice(tokens, payload);
   });
